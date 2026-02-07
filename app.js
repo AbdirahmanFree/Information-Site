648502c8dc37f0a61a7adcb37f9d13e0bc0fc9de
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2026-02-07 17:49:49 -0500

    finsihed routing

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-const { createServer } = require('node:http');
+const { createServer } = require('node:http')
 const fs = require('fs')
 
-const hostname = '127.0.0.1';
-const port = 5000;
 
-const server = createServer((req, res) => {
-    if(req.url = '/home'){
-        res.statusCode = 200
-        res.setHeader('content-type', 'text/html')
+const port = 5000;
+const hostname ='localhost'
+
+const server = createServer((req,res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    console.log(req.url)
+    if(req.url == '/'){
         res.end(fs.readFileSync('./index.html'))
+        res.statusCode = 200
+    }
+    else if(req.url == '/about'){
+        res.end(fs.readFileSync('./about.html'))
+    }
+    else if(req.url == '/contact'){
+        res.end(fs.readFileSync('./contact-me.html'))
     }
     else {
-        res.statusCode = 404
-        res.setHeader('content-type', 'text/html');
         res.end(fs.readFileSync('./404.html'))
+        res.statusCode = 404
     }
-
-    
+ 
 })
 
-server.listen(port,hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port,hostname, () =>{
+    console.log(`server running on http://${hostname}:${port}/`)
 })
